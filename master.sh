2db888af23c8a4033f70e5f3da573c7905386459
JanStreffing <jan.streffing@awi.de>
2020-03-22 21:27:27 +0100

    modify epflux to plots T1279 correctly

--- master.sh
+++ master.sh
@@ -5,3 +5,5 @@ out=/p/project/chhb19/jstreffi/postprocessing/PAMIP
 
 cd $in
 ncl /p/project/chhb19/jstreffi/postprocessing/ncl-plot-scripts/epflux.ncl res="159" exp1="11" exp2="16"
+ncl /p/project/chhb19/jstreffi/postprocessing/ncl-plot-scripts/epflux.ncl res="511" exp1="11" exp2="16"
+ncl /p/project/chhb19/jstreffi/postprocessing/ncl-plot-scripts/epflux.ncl res="1279" exp1="11" exp2="16"
